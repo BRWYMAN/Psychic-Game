c5ecf06754d20bec9c3252cc97ecae16e117a584
John Locke <brw7677@gmail.com>
2018-10-09 01:08:48 -0400

    <changes>

---
+++ assets/javascript/game.js
@@ -0,0 +1,70 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// console.log(letters.length);
+
+letters.forEach(function [i]) {
+console.log[i]
+};
+// var lettersLength = letters.length;
+
+function consoleInside(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+
+}
+console.log("---------")
+
+consoleInside(letters);
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+var directionsText = document.getElementById("directions-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var tiesText = document.getElementById("ties-text");
+
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+
+        if ((userGuess === "r") && (computerGuess === "s")) {
+            wins++;
+        } else if ((userGuess === "r") && (computerGuess === "p")) {
+            losses++;
+        } else if ((userGuess === "s") && (computerGuess === "r")) {
+            losses++;
+        } else if ((userGuess === "s") && (computerGuess === "p")) {
+            wins++;
+        } else if ((userGuess === "p") && (computerGuess === "r")) {
+            wins++;
+        } else if ((userGuess === "p") && (computerGuess === "s")) {
+            losses++;
+        } else if (userGuess === computerGuess) {
+            ties++;
+        }
+
+        console.log(win + " " + loss + " " + tie);
+        directionsText.textContent = ""; //hide the directions
+        userChoiceText.textContent = "You chose:" + userGuess;
+        computerChoiceText.textContent = "The computer chose:" + computerGuess
+        winsText.textContent = "wins: " + wins;
+        lossesText.textContent = "losses: " + losses;
+        tiesText.textContent = "ties: " + ties;
+    }
+    if (userGuess === computerGuess) {
+        win++;
+    } else if (userGuess !== computerGuess) {
+        guessesleft--;
+    }
+
+};
